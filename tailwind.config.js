ea09dd1c80748b7e1037007fab02aaf8fe806565
Shajid <shajid1biz@gmail.com>
2023-10-06 16:28:00 +0530

    Added worksans font to the body

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ['"Work Sans"']
+      }
+    },
   },
   plugins: [],
 }
